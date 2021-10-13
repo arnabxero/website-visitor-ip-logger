89ce9416a60025b81bee02bdeeb8c577e3d3c9bd
arnab-xero <62126014+arnab-xero@users.noreply.github.com>
2021-10-13 10:47:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ $ip=$_SERVER['REMOTE_ADDR'];
 //Open Last Visitor's File to replace data to current visitor
 $file = fopen('last_visitor.txt','w');
 
+
+//Content Type Imgage
+header("Content-type: image/svg+xml");
+
 //Open Main IP Log File
 $file2 = fopen('Total_IP_Log.txt', 'a');
 //Add current visitor to IP Log
@@ -18,10 +22,10 @@ $today = new DateTime("now", new DateTimeZone('Asia/Dhaka') );
 
 //Print Data On Webpage
 //Print visitor ip
-echo "Your IP Address is $ip ";
+//echo "Your IP Address is $ip ";
 //Print Current Date And Time
-echo "BD Time - ";
-echo $today->format('Y/m/d - h:i:sa');
+//echo "BD Time - ";
+//echo $today->format('Y/m/d - h:i:sa');
 
 //Adding Time Date and Zone to Log
 $saveTime =  $today->format('Y/m/d          h:i:sa');
@@ -30,4 +34,32 @@ fwrite($file2,"          ".$saveTime."          BDT");
 //Writing Last visitor file
 fwrite($file,'Last Visitor Was '.$ip.", Time Stamp : ".$saveTime." BDT");
 
+// get contents of a URL with curl
+function curl_get_contents($url): string
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+// set parameters for the shields.io URL
+$params = [
+    "label" => $ip,
+    "logo" => "none",
+    "message" => $saveTime,
+    "color" => "purple",
+];
+
+// build the URL with an SVG image of the view counter
+$url = "https://img.shields.io/static/v1?" . http_build_query($params);
+
+// output the response (svg image)
+echo curl_get_contents($url);
  ?> 
